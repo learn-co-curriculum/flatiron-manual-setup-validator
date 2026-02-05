c86552255399c41105d6b0358fb7393bc94e03df
Laura Berge <lauraeberge@gmail.com>
2026-02-05 10:44:42 -0600

    fix hardcoded node version check for WSL

--- wsl-phase-0-manual-setup-validator-with-py.sh
+++ wsl-phase-0-manual-setup-validator-with-py.sh
@@ -65,9 +65,12 @@ delimiter
 . ~/.nvm/nvm.sh
 print_table_results "Installed NVM" "command -v nvm >/dev/null 2>&1 && nvm --version | grep -q '[0-9]*\.[0-9]*\.[0-9]*'"
 print_table_results "Installed Node" "command -v node | grep -q '.nvm/versions/node'"
-print_table_results "Default Node (>11.x)" 'command -v nvm >/dev/null 2>&1 && nvm version default | grep -q "v11\|v12\|v13\|v14\|v15\|v16\|v17\|v18\|v19\|v20"'
-# print_table_results "Default Node (10.x)" 'command -v nvm >/dev/null 2>&1 && nvm version default | grep -q "v10"'
-# print_table_results "Default Node (6.11.2)" 'command -v nvm >/dev/null 2>&1 && nvm version default | grep -q "v6.11.2"'
+print_table_results "Default Node (>=18)" '
+  command -v nvm >/dev/null 2>&1 &&
+  v="$(nvm version default 2>/dev/null)" &&
+  [[ "$v" =~ ^v([0-9]+)\. ]] &&
+  (( ${BASH_REMATCH[1]} >= 18 ))
+'
 delimiter
 
 ## 3. Python
